9741a7caf739bb3621d46b59dbc0c4aab4aedd14
SeungJin051 <seungjin051@gmail.com>
2025-02-12 20:14:11 +0900

    feat: 프로젝트 설정

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+	baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+	{
+		files: ['**/*.js', '**/*.jsx'],
+	},
+	...compat.config({
+		extends: ['next/core-web-vitals', 'plugin:prettier/recommended'],
+		plugins: ['prettier'],
+	}),
+];
+
+export default eslintConfig;
