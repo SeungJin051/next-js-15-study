f6c9ab673a7e8bd15136995209e024c3ae616ed8
SeungJin051 <seungjin051@gmail.com>
2025-02-15 01:09:34 +0900

    feat: private folder(_폴더)처리, 컴포넌트 분리

--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -1,7 +1,16 @@
-import { redirect } from 'next/navigation';
+'use client';
 
-const Login = () => {
-  redirect('/i/flow/login');
-};
+import { useRouter } from 'next/navigation';
+import Main from '@/app/(beforeLogin)/_component/Main';
+import { useEffect } from 'react';
 
-export default Login;
+export default function Login() {
+  const router = useRouter();
+
+  // 렌더링 중  router.replace X -> useEffect 처리
+  useEffect(() => {
+    router.replace('/i/flow/login');
+  }, [router]);
+
+  return <Main />;
+}
